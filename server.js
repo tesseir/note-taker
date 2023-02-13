c201bd98ad256405f16af0e07ef883e5eef4523b
tesseir <tesseir.edu@gmail.com>
2023-02-12 18:40:14 -0600

    added basic dependencies and html routes

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const fs = require("fs");
 const util = require('util');
+const path = require('path');
+const PORT = process.evn.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
